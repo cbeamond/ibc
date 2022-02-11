5367ce3aa30ca09abf19b0cb668716e83dd3e1ad
Christopher Beamond <chris.beamond@illuvium.io>
2022-02-11 12:39:13 -0300

    POC of the object pool

---
+++ Assets/Scripts/Utility/ObjectPooler.ObjectPool.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityObject = UnityEngine.Object;
+
+public partial class ObjectPooler
+{
+	private class UnityObjectPool<T> where T : UnityObject
+	{
+		private readonly T objectToClone;
+		private readonly List<T> objects = new List<T>();
+		private readonly Stack<int> stack = new Stack<int>();
+
+		public UnityObjectPool(T objectToClone, int poolSize)
+		{
+			this.objectToClone = objectToClone;
+			Resize(poolSize);
+		}
+
+		public void Resize(int poolSize)
+		{
+			int originalObjectCount = objects.Count;
+
+			if (poolSize > originalObjectCount)
+			{
+				for (int i = originalObjectCount; i < poolSize; i++)
+				{
+					objects.Add(UnityObject.Instantiate(objectToClone));
+					stack.Push(i);
+				}
+				return;
+			}
+
+			// there are two options here.
+			//	1 - remove only the end of the list. This may involve removing active objects that don't need to be removed
+			//  2 - remove inactive objects first, and then the end of the list. Much better!
+			// we also have to destroy any removed active objects
+			throw new NotImplementedException();
+		}
+
+		public void Destroy() => Resize(0);
+	}
+}
---
+++ Assets/Scripts/Utility/ObjectPooler.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityObject = UnityEngine.Object;
+
+//add logging!!!!
+
+public partial class ObjectPooler
+{
+	private readonly Dictionary<Type, UnityObjectPool<UnityObject>> unityObjectPools
+		= new Dictionary<Type, UnityObjectPool<UnityObject>>();
+
+	public void CreatePool<T>(T objectToClone, int poolSize) where T : UnityObject
+	{
+		Type poolType = typeof(T);
+		if (!unityObjectPools.ContainsKey(poolType))
+			unityObjectPools.Add(poolType, new UnityObjectPool<T>(objectToClone, poolSize));
+
+		ResizePool<T>(poolSize);
+	}
+
+	public void ResizePool<T>(int poolSize)
+	{
+		if (unityObjectPools.TryGetValue(typeof(T), out var pool))
+			pool.Resize(poolSize);
+	}
+
+	public void DestroyPool<T>()
+	{
+		Type poolType = typeof(T);
+
+		if (unityObjectPools.TryGetValue(poolType, out var pool))
+		{
+			pool.Destroy();
+			unityObjectPools.Remove(poolType);
+		}
+	}
+}
