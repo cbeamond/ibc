b8cedba6c98bccbe8f3843f04a92af1e4e7a4bf8
Christopher Beamond <chris.beamond@illuvium.io>
2022-02-15 02:08:02 -0300

    Cherry Picked threaded nearest neighbour pool

---
+++ Assets/Scripts/Utility/ThreadedNearestNeighbour/ThreadedFindNearestNeighbourSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Ironbelly.Systems
+{
+	public class ThreadedFindNearestNeighbour : MonoBehaviour
+	{
+		public ThreadedFindNearestNeighbour nearestNeighbour =>
+			ThreadedFindNearestNeighbourSystem.Instance.GetNearestNeighbour(this);
+
+		public void OnEnable()
+		{
+			ThreadedFindNearestNeighbourSystem.Instance.AddNeighbour(this);
+		}
+
+		public void OnDisable()
+		{
+			ThreadedFindNearestNeighbourSystem.Instance.RemoveNeighbour(this);
+		}
+	}
+}
